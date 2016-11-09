c9ebc3f73954b3a842f1f8d898c8028683721468
mombi93 <m.d.a.rafdi@gmail.com>
2016-11-09 09:57:27 +0000

    Added new route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/:name', function(req, res, next) {
+  res.send('hello ' + req.params.name);
+});
+
 module.exports = router;
